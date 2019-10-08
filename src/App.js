8e2583ae92239e1f86fb1a3a534cd4a05d3a7e27
smeebear <soheron@gmail.com>
2019-10-08 12:55:11 -0500

    Added loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ExampleQuiz from './components/ExampleQuiz'
 import './App.css';
 import NavBar from './components/NavBar'
@@ -12,27 +12,50 @@ import Learning from './components/learning/Learning'
 import Video from './components/learning/Video';
 import Reading from './components/learning/Reading';
 import Speaking from './components/learning/Speaking';
+import LoadingScreen from 'react-loading-screen';
 
-function App() {
-  return (
-    <Router>
-      <NavBar title={ "Aristotle" } />
-      <div style={{margin: 'auto', maxWidth: '1000px'}} >
-      <Route exact path='/' component={ExampleQuiz} />
-      <Route path='/results' component={ResultsVert} />
-      <Route path='/contact' component={Contact} />
-      <Route path='/thanks' component={ThankYou} />
-      <Route path='/parentdash' component={Parent} />
-      <Route path='/teacherdash' component={Teacher} />
-      <Route path='/learning' component={Learning} />
-      <Route path='/learning/visual' component={Video} />
-      <Route path='/learning/aural' component={Reading} />
-      <Route path='/learning/verbal' component={Speaking} />
-      <Route path='/learning/physical' component={Speaking} />
-      <Route path='/learning/logical' component={Reading} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentDidMount(){
+
+    this.setState((state, props) => {
+      return {loading: false}
+    })
+  }
+
+  render (){
+    return (
+      <LoadingScreen
+        loading={this.state.loading}
+        logoSrc='./img/logo.png'
+      >
+        <Router>
+          <NavBar title={ "Aristotle" } />
+          <div style={{margin: 'auto', maxWidth: '1000px'}} >
+          <Route exact path='/' component={ExampleQuiz} />
+          <Route path='/results' component={ResultsVert} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/thanks' component={ThankYou} />
+          <Route path='/parentdash' component={Parent} />
+          <Route path='/teacherdash' component={Teacher} />
+          <Route path='/learning' component={Learning} />
+          <Route path='/learning/visual' component={Video} />
+          <Route path='/learning/aural' component={Reading} />
+          <Route path='/learning/verbal' component={Speaking} />
+          <Route path='/learning/physical' component={Speaking} />
+          <Route path='/learning/logical' component={Reading} />
+          </div>
+        </Router>
+      </LoadingScreen>
+    );
+  }
 }
 
 export default App;
